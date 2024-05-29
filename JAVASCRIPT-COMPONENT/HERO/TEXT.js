7c4383c5d5d6402408a1f6a10c86c508a17968de
samedy-leader <fordancestyle@gmail.com>
2024-05-29 10:03:24 +0700

    created components

--- JAVASCRIPT-COMPONENT/HERO/TEXT.js
+++ JAVASCRIPT-COMPONENT/HERO/TEXT.js
@@ -1,8 +1,44 @@
 "use strict";
 
-// text over hero image 1 > strong
-const text1 = document.querySelector(".text-over-hero-img strong");
-text1.textContent = "iPhone 15 Pro";
-// text over hero image 1 > h3
-const text1H3 = document.querySelector(".text-over-hero-img h3");
-text1H3.textContent = "Titanium. So strong. So light. So Pro.";
+// text over hero image strong
+const textStrong = document.getElementsByClassName("text-over-hero-img");
+const textStrongArray = [...textStrong];
+const h3SelectaArray = [...document.querySelectorAll('.text-over-hero-img h3')]
+const strongCopiedArray = textStrongArray.map(element => element.firstElementChild)
+
+textStrongArray.forEach((text, index) => {
+    // strong elements
+    const strongArray = [
+    'iPhone 15 Pro',
+    'iPhone 15',
+    'MacBook Air',
+    ' ',
+    'Vision Pro',
+    'WATCH',
+    'iPad',
+    'Card'
+    ]
+    text.firstElementChild.textContent = strongArray[index];
+
+    // h3 elements
+    const h3Array = ['Titanium. So strong. So light. So Pro.',
+    'New Camera. New Design. Newphoria.',
+    'Lean. Mean. M3 machine.',
+    'Apple Worldwide Developers Conference Join us online June 10-14.',
+    'Welcome to the era of spatial computing.',
+    'Smart. Brightier. Mightier',
+    'Lovable. Drawable. Magical.',
+    'Get up to 3% Daily Cash back with every purchase.'
+    ]
+    for (let i = 0; i < h3SelectaArray.length; i++) {
+        h3SelectaArray[i].textContent = h3Array[i];
+    }
+})
+
+strongCopiedArray.forEach((element, index) => {
+    for (let j = 0; j < strongCopiedArray.length; j++) {
+        if (index >= 4 && !element.querySelector('.apple-logo')) {
+            element.insertAdjacentHTML('afterBegin', '<i class="apple-logo ri-apple-fill"></i>')
+        }
+    }
+})
--- script.js
+++ script.js
@@ -9,23 +9,131 @@ function updateHeroImage() {
 
   arrayHeroImg.forEach((image) => {
     if (screenWidth >= 940) {
+      // iphone 15 pro
       arrayHeroImg[0].setAttribute(
         "src",
         "./assets/images/IPhone/hero-ip15-new.jpg"
       );
+      // iphone 15
       arrayHeroImg[1].setAttribute(
         "src",
         "./assets/images/IPhone/hero_iphone15_announce__uuemlcwczn6u_largetall_2x.jpg"
       );
+      // iphone 15 but alt
+      arrayHeroImg[1].setAttribute(
+        "alt",
+        "iPhone 15"
+      );
+      // macbook air
+      arrayHeroImg[2].setAttribute(
+        "src",
+        "./assets/images/Macbook/promo_macbook_air_m3__e43jegok3wuq_large_2x.jpg"
+      )
+      // macbook air but alt
+      arrayHeroImg[2].setAttribute(
+        "alt",
+        "MacBook Air"
+      )
+      // grid wwdc2024
+      arrayHeroImg[3].setAttribute(
+        "src",
+        "./assets/images/Promo/WWDC/promo_wwdc24_announce__fv0p5miwnjiq_large_2x.jpg"
+      )
+      arrayHeroImg[3].setAttribute(
+        "alt",
+        "WWDC 2024"
+      )
+      // grid apple vision
+      arrayHeroImg[4].setAttribute(
+        "src",
+        "./assets/images/Promo/Vision Pro/apple-vision-pro.jpg"
+      )
+      // grid apple vision but alt
+      arrayHeroImg[4].setAttribute(
+        "alt",
+        "Apple Vision Pro"
+      )
+      // grid apple smart watch
+      arrayHeroImg[5].setAttribute(
+        "src",
+        "./assets/images/Promo/Watch/promo_apple_watch_series_9_order__b3u85rm9zf6u_large_2x.jpg"
+      )
+      // grid apple smart watch but alt
+      arrayHeroImg[5].setAttribute(
+        "alt",
+        "Apple Smart Watch"
+      )
+      // grid apple ipad
+      arrayHeroImg[6].setAttribute(
+        "src",
+        "./assets/images/Promo/Ipad/promo_ipad__fioegapg12qi_large_2x-1.jpg"
+      )
+      // grid apple ipad but alt
+      arrayHeroImg[6].setAttribute(
+        "alt",
+        "iPad Pro"
+      )
+      // grid apple card
+      arrayHeroImg[7].setAttribute(
+        "src",
+        "./assets/images/Promo/Card/tile__cauwwcyyn9hy_large_2x.jpg"
+      )
+      // grid apple card but alt
+      arrayHeroImg[7].setAttribute(
+        "alt",
+        "iCard"
+      )
+      // grid apple trade-in
+      arrayHeroImg[8].setAttribute(
+        "src",
+        "./assets/images/Promo/Trade in/promo_iphone_tradein__bugw15ka691e_large_2x.jpg"
+      )
+      // grid apple trade-in but alt
+      arrayHeroImg[8].setAttribute(
+        "alt",
+        "Apple Trade-in"
+      )
     } else {
+      // iphone 15 pro
       arrayHeroImg[0].setAttribute(
         "src",
         "./assets/images/IPhone/For mobile/hero_iphone15pro__i70z9oz3hj2i_small_2x.jpg"
       );
+      // iphone 15
       arrayHeroImg[1].setAttribute(
         "src",
-        "./assets/images/IPhone/hero-iphone15.jpg"
+        "./assets/images/IPhone/hero_iphone15_announce__uuemlcwczn6u_largetall_2x.jpg"
       );
+      // macbook air
+      arrayHeroImg[2].setAttribute(
+        "src",
+        "./assets/images/Macbook/promo_macbook_air_m3__e43jegok3wuq_small_2x.jpg"
+      )
+      // grid wwdc 2024
+      arrayHeroImg[3].setAttribute(
+        "src",
+        "./assets/images/Promo/WWDC/promo_wwdc24_announce__fv0p5miwnjiq_small_2x.jpg"
+      )
+      // grid apple vision
+      arrayHeroImg[4].setAttribute(
+        "src",
+        "./assets/images/Promo/Vision Pro/apple-vision-pro-small.jpg"
+      )
+      // grid apple smart watch but alt
+      arrayHeroImg[5].setAttribute(
+        "src",
+        "./assets/images/Promo/Watch/promo_apple_watch_series_9_order__b3u85rm9zf6u_small_2x.jpg"
+      )
+      // grid apple card
+      arrayHeroImg[7].setAttribute(
+        "src",
+        "./assets/images/Promo/Card/tile__cauwwcyyn9hy_small_2x.jpg"
+      )
+      // grid apple trade-in
+      arrayHeroImg[8].setAttribute(
+        "src",
+        "./assets/images/Promo/Trade in/promo_iphone_tradein__bugw15ka691e_small_2x.jpg"
+      )
     }
   });
 }
