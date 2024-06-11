c85066a54f7ee6d0be8a14230333dbbd69872f83
Samedy <Samedy.hunx@gmail.com>
2024-06-11 21:50:08 +0700

    feature/#5: fixed find-a-store page

--- javascript/script.js
+++ javascript/script.js
@@ -1,125 +1,129 @@
 "use strict";
 
-(function() {
-// all the hero-image elements
-const arrayHeroImg = Array.from(document.getElementsByClassName("hero-img"));
-// array to hold all the photo src and alt attributes
-const heroImgAttributeArray = [
-  [['src', "./assets/images/IPhone/hero-ip15-new.jpg"], ['alt', 'iPhone 15 Pro'], ["src", "./assets/images/IPhone/For mobile/hero_iphone15pro__i70z9oz3hj2i_small_2x.jpg"]],
-  [['src', "./assets/images/IPhone/hero_iphone15_announce__uuemlcwczn6u_largetall_2x.jpg"], ['alt', 'iPhone 15'], ["src", "./assets/images/IPhone/hero_iphone15_announce__uuemlcwczn6u_largetall_2x.jpg"]],
-  [['src', "./assets/images/Macbook/promo_macbook_air_m3__e43jegok3wuq_large_2x.jpg"], ['alt', 'MacBook Air'], ['src', "./assets/images/Macbook/promo_macbook_air_m3__e43jegok3wuq_small_2x.jpg"]],
-  [['src', "./assets/images/Promo/WWDC/promo_wwdc24_announce__fv0p5miwnjiq_large_2x.jpg"], ['alt', 'WWDC 2024'], ['src', "./assets/images/Promo/WWDC/promo_wwdc24_announce__fv0p5miwnjiq_small_2x.jpg"]],
-  [['src', "./assets/images/Promo/Vision Pro/apple-vision-pro.jpg"], ['alt', 'Apple Vision Pro'], ['src', "./assets/images/Promo/Vision Pro/apple-vision-pro-small.jpg"]],
-  [['src', "./assets/images/Promo/Watch/promo_apple_watch_series_9_order__b3u85rm9zf6u_large_2x.jpg"], ['alt', 'Apple Smart Watch'], ['src', "./assets/images/Promo/Watch/promo_apple_watch_series_9_order__b3u85rm9zf6u_small_2x.jpg"]],
-  [['src', "./assets/images/Promo/Ipad/promo_ipad__fioegapg12qi_small_2x.jpg"], ['alt', 'iPad'], ['src', "./assets/images/Promo/Ipad/promo_ipad__fioegapg12qi_small_2x.jpg"]],
-  [['src', "./assets/images/Promo/Card/tile__cauwwcyyn9hy_large_2x.jpg"], ['alt', 'iCard'], ['src', "./assets/images/Promo/Card/tile__cauwwcyyn9hy_small_2x.jpg"]],
-  [['src', "./assets/images/Promo/Trade in/promo_iphone_tradein__bugw15ka691e_large_2x.jpg"], ['alt', 'Apple Trade-in'], ['src', "./assets/images/Promo/Trade in/promo_iphone_tradein__bugw15ka691e_small_2x.jpg"]]
-];
+// using iife here.
+(function () {
+  // all the hero-image elements
+  const arrayHeroImg = Array.from(document.getElementsByClassName("hero-img"));
 
-// adjustment to make the photo appear differently in a different screen-width
-function updateHeroImage() {
-  const screenWidth = window.innerWidth;
-  arrayHeroImg.forEach((image, i) => {
-    if (screenWidth >= 1140) {
-      image.setAttribute(heroImgAttributeArray[i][0][0], heroImgAttributeArray[i][0][1]);
-      image.setAttribute(heroImgAttributeArray[i][1][0], heroImgAttributeArray[i][1][1]);
-    } else {
-      image.setAttribute(heroImgAttributeArray[i][2][0], heroImgAttributeArray[i][2][1]);
-    }
-  });
-}
+  // adjustment to make the photo appear differently in a different screen-width
+  function updateHeroImage() {
+    const screenWidth = window.innerWidth;
+    arrayHeroImg.forEach((image, i) => {
+      if (screenWidth >= 1140) {
+        image.setAttribute(
+          heroImgAttributeArray[i][0][0],
+          heroImgAttributeArray[i][0][1]
+        );
+        image.setAttribute(
+          heroImgAttributeArray[i][1][0],
+          heroImgAttributeArray[i][1][1]
+        );
+      } else {
+        image.setAttribute(
+          heroImgAttributeArray[i][2][0],
+          heroImgAttributeArray[i][2][1]
+        );
+      }
+    });
+  }
 
-updateHeroImage();
+  updateHeroImage();
 
-window.addEventListener("resize", updateHeroImage);
-// marking the end of hero component styling..
+  window.addEventListener("resize", updateHeroImage);
+  // marking the end of hero component styling..
 })();
 
 // button deletion for grid section
-const buttonLearnMore = [...document.getElementsByClassName('learn-more')];
-const buttonBuy = [...document.getElementsByClassName('buy')];
+const buttonLearnMore = [...document.getElementsByClassName("learn-more")];
+const buttonBuy = [...document.getElementsByClassName("buy")];
 
-buttonBuy[7].textContent = 'Apply now';
+buttonBuy[7].textContent = "Apply now";
 
-buttonLearnMore[8].textContent = 'Get your estimate';
-buttonLearnMore[8].style.color = 'white';
+buttonLearnMore[8].textContent = "Get your estimate";
+buttonLearnMore[8].style.color = "white";
 
 buttonBuy[3].remove();
 buttonBuy[8].remove();
 // marking the end of button deletion for the grid section..
 
 // text over hero image strong
-const textStrongArray = [...document.getElementsByClassName("text-over-hero-img")];
-const strongCopiedArray = textStrongArray.map(element => element.firstElementChild);
-const h3SelectaArray = [...document.querySelectorAll('.text-over-hero-img h3')];
+const textStrongArray = [
+  ...document.getElementsByClassName("text-over-hero-img"),
+];
+const strongCopiedArray = textStrongArray.map(
+  (element) => element.firstElementChild
+);
+const h3SelectaArray = [...document.querySelectorAll(".text-over-hero-img h3")];
 // external apple logo
 const appleLogo = '<i class="apple-logo ri-apple-fill"></i>';
 
 textStrongArray.forEach((text, index) => {
-    // strong elements
-    const strongArray = [
-    'iPhone 15 Pro',
-    'iPhone 15',
-    'MacBook Air',
-    ' ',
-    'Vision Pro',
-    'WATCH',
-    'iPad',
-    'Card',
-    'Trade In',
-    ]
-    text.firstElementChild.textContent = strongArray[index];
+  // strong elements
+  const strongArray = [
+    "iPhone 15 Pro",
+    "iPhone 15",
+    "MacBook Air",
+    " ",
+    "Vision Pro",
+    "WATCH",
+    "iPad",
+    "Card",
+    "Trade In",
+  ];
+  text.firstElementChild.textContent = strongArray[index];
 
-    // h3 elements
-    const h3Array = ['Titanium. So strong. So light. So Pro.',
-    'New Camera. New Design. Newphoria.',
-    'Lean. Mean. M3 machine.',
-    'Apple Worldwide Developers Conference Join us online June 10-14.',
-    'Welcome to the era of spatial computing.',
-    'Smart. Brightier. Mightier',
-    'Lovable. Drawable. Magical.',
-    'Get up to 3% Daily Cash back with every purchase.',
-    'Get $170-$630 in credit when you trade in iPhone 11 or higher.'
-    ]
-    for (let i = 0; i < h3SelectaArray.length; i++) {
-        h3SelectaArray[i].textContent = h3Array[i];
-    }
-})
+  // h3 elements
+  const h3Array = [
+    "Titanium. So strong. So light. So Pro.",
+    "New Camera. New Design. Newphoria.",
+    "Lean. Mean. M3 machine.",
+    "Apple Worldwide Developers Conference Join us online June 10-14.",
+    "Welcome to the era of spatial computing.",
+    "Smart. Brightier. Mightier",
+    "Lovable. Drawable. Magical.",
+    "Get up to 3% Daily Cash back with every purchase.",
+    "Get $170-$630 in credit when you trade in iPhone 11 or higher.",
+  ];
+  for (let i = 0; i < h3SelectaArray.length; i++) {
+    h3SelectaArray[i].textContent = h3Array[i];
+  }
+});
 // marking the end of the grid section styling..
 
 // black background so do not have to change strong, h3, button background to black or white
 // 1 indicates black and 0 represents white
-const excludedIndexes = [1, 0, 0, 1, 1, 1, 0, 0, 0]
+const excludedIndexes = [1, 0, 0, 1, 1, 1, 0, 0, 0];
 excludedIndexes.forEach((element, i) => {
-    if (element === 1) {
-        h3SelectaArray[i].style.color = 'white';
-        strongCopiedArray[i].style.color = 'white';
-        buttonBuy[i].style.backgroundColor = 'black';
-    } else {
-        h3SelectaArray[i].style.color = 'black';
-        strongCopiedArray[i].style.color = 'black';
-        buttonBuy[i].style.backgroundColor = 'white';
-    }
-})
+  if (element === 1) {
+    h3SelectaArray[i].style.color = "white";
+    strongCopiedArray[i].style.color = "white";
+    buttonBuy[i].style.backgroundColor = "black";
+  } else {
+    h3SelectaArray[i].style.color = "black";
+    strongCopiedArray[i].style.color = "black";
+    buttonBuy[i].style.backgroundColor = "white";
+  }
+});
 // marking the end
 
 strongCopiedArray.forEach((element, index) => {
-    for (let j = 0; j < strongCopiedArray.length; j++) {
-        if (index >= 4 && !element.querySelector('.apple-logo')) {
-            element.insertAdjacentHTML('afterBegin', appleLogo)
-        }
+  for (let j = 0; j < strongCopiedArray.length; j++) {
+    if (index >= 4 && !element.querySelector(".apple-logo")) {
+      element.insertAdjacentHTML("afterBegin", appleLogo);
     }
+  }
 });
 
-const loginPageArray = [...document.getElementsByClassName('nav-items-component')];
+const loginPageArray = [
+  ...document.getElementsByClassName("nav-items-component"),
+];
 
-loginPageArray.forEach(login => {
-  login.lastElementChild.classList.add('right-group');
-})
+loginPageArray.forEach((login) => {
+  login.lastElementChild.classList.add("right-group");
+});
 
-const rightGroupArray = [...document.getElementsByClassName('right-group')];
+const rightGroupArray = [...document.getElementsByClassName("right-group")];
 
-rightGroupArray.forEach(right => {
-  right.children[1].setAttribute('href', "/signin.html")
-})
-
+rightGroupArray.forEach((right) => {
+  right.children[1].setAttribute("href", "/signin.html");
+});
--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -4,59 +4,9 @@
 const slideshowImg = [
   ...document.querySelectorAll(".slideshow .image-slider img"),
 ];
-const sliderImgArray = [
-  "./assets/images/Promo/Image Slider/Argylle/Argylle Small.jpg",
-  "./assets/images/Promo/Image Slider/Franklin/Franklin Small.jpg",
-  "./assets/images/Promo/Image Slider/Master of Air/Master of AIR SMALL.jpg",
-  "./assets/images/Promo/Image Slider/Sugar/Sugar Small.jpg",
-  "./assets/images/Promo/Image Slider/Ted Lasso/Ted Lasso small.jpg",
-  "./assets/images/Promo/Image Slider/Argylle/Argylle Small.jpg",
-  "./assets/images/Promo/Image Slider/Franklin/Franklin Small.jpg",
-  "./assets/images/Promo/Image Slider/Master of Air/Master of AIR SMALL.jpg",
-  "./assets/images/Promo/Image Slider/Sugar/Sugar Small.jpg",
-  "./assets/images/Promo/Image Slider/Ted Lasso/Ted Lasso small.jpg",
-];
-const sliderImgAlt = [
-  "Argylle",
-  "Franklin",
-  "Master of Air",
-  "Sugar",
-  "Ted Lasso",
-  "Argylle",
-  "Franklin",
-  "Master of Air",
-  "Sugar",
-  "Ted Lasso",
-];
 // slider section
 const sliderImg = [
   ...document.querySelectorAll(".my-slider.carousel-slider .image-slider img"),
-];
-
-const sliderImgSmallerArray = [
-  "./assets/images/Carousal/Heartstring/Small.jpg",
-  "./assets/images/Carousal/Helly Kitty Island Advanture/Small.jpg",
-  "./assets/images/Carousal/NBA 2k24 Arcade Edition/Small.jpg",
-  "./assets/images/Carousal/Superbloom/small.jpg",
-  "./assets/images/Carousal/Additional/Mobile/228x228SC.DN01.jpg",
-  "./assets/images/Carousal/Heartstring/Small.jpg",
-  "./assets/images/Carousal/Helly Kitty Island Advanture/Small.jpg",
-  "./assets/images/Carousal/NBA 2k24 Arcade Edition/Small.jpg",
-  "./assets/images/Carousal/Superbloom/small.jpg",
-  "./assets/images/Carousal/Additional/Mobile/228x228SC.DN01.jpg",
-];
-
-const sliderImgAltSmallerArray = [
-  "Heartstring",
-  "Hello Kitty World",
-  "NBA 2k24",
-  "Superbloom",
-  "A-List Pop",
-  "Heartstring",
-  "Hello Kitty World",
-  "NBA 2k24",
-  "Superbloom",
-  "A-List Pop",
 ];
 
 for (let i = 0; i < slideshowImg.length; i++) {
